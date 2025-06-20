f5c00cd96d96b2e9cf1c7a598e3f8083a5b90581
MOHAMED-INSAF <158418205+MOHAMED-INSAF@users.noreply.github.com>
2025-06-20 13:54:03 +0530

    del

--- backend/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Database configuration
-SQLALCHEMY_DATABASE_URI = 'sqlite:///optimoto.db'  # Using SQLite for simplicity
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-# JWT configuration
-JWT_SECRET_KEY = 'your-secret-key-here'  # Change this to a secure secret key 
